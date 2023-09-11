ea389f8587b37105ff38bd068a4d27fec7fcb2c0
sara-dsn <sabr.tdg@gmail.com>
2023-09-11 14:15:03 +0200

    first commit

---
+++ ex1.js
@@ -0,0 +1,13 @@
+var a;
+window.prompt("Entrez un nombre: ");
+window.alert(a);
+if (a%2==0)
+{
+    console.log("C'est une paire");
+    windows.alert(console.log)
+    
+}
+else
+{
+    window.console.log("Ce n'est pas une paire");
+}
