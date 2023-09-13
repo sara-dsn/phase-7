8b978bfc31420cd65a8c34960e74c74d108c55fc
sara-dsn <sabr.tdg@gmail.com>
2023-09-13 10:39:08 +0200

    phase 7

--- ex3.js
+++ ex3.js
@@ -1,29 +1,29 @@
 var n1=window.prompt("Entrez le premier nombre à calculer: ");
 var n2=window.prompt("Entrez le second nombre: ");
 var signe=window.prompt("Entrez l'opérateur à exécuter(+,-,*,/): ");
-if (signe="+")
+if (signe='+')
 {
-    resultat=(n1+n2);
+    var resultat=(n1+n2);
     console.log(resultat);
     window.alert(resultat);
 }
-if (signe="-")
+if (signe='-')
 {
-    resultat=n1-n2;
-    console.log(resultat);
-    window.alert(resultat);
+    var resultat1=n1-n2;
+    console.log(resultat1);
+    window.alert(resultat1);
 }
 if (signe="*")
 {
-    resultat=n1*n2;
-    console.log(resultat);
-    window.alert(resultat);
+    var resultat2=n1*n2;
+    console.log(resultat2);
+    window.alert(resultat2);
 }
 if (signe="/")
 {
-    resultat=n1/n2;
-    console.log(resultat);
-    window.alert(resultat);
+    var resultat3=n1/n2;
+    console.log(resultat3);
+    window.alert(resultat3);
 }
 else
 {
