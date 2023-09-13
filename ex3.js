01dc0eab8c17397edc1601ce8a2339743876d344
sara-dsn <sabr.tdg@gmail.com>
2023-09-13 13:36:41 +0200

    phase 7

--- ex3.js
+++ ex3.js
@@ -1,29 +1,33 @@
-var n1=window.prompt("Entrez le premier nombre à calculer: ");
-var n2=window.prompt("Entrez le second nombre: ");
+var n1=parseInt(window.prompt("Entrez le premier nombre à calculer: "));
+var n2=parseInt(window.prompt("Entrez le second nombre: "));
+
+// alert(typeof(n1));
+// alert(typeof(n2));
+
 var signe=window.prompt("Entrez l'opérateur à exécuter(+,-,*,/): ");
-if (signe='+')
+if(signe=='+')
 {
-    var resultat=(n1+n2);
-    console.log(resultat);
-    window.alert(resultat);
+    var addi=(n1+n2);
+    console.log(addi);
+    window.alert(addi);
 }
-if (signe='-')
+else if (signe=='-')
 {
-    var resultat1=n1-n2;
-    console.log(resultat1);
-    window.alert(resultat1);
+    var sous=n1-n2;
+    console.log(sous);
+    window.alert(sous);
 }
-if (signe="*")
+else if (signe=="*")
 {
-    var resultat2=n1*n2;
-    console.log(resultat2);
-    window.alert(resultat2);
+    var multi=n1*n2;
+    console.log(multi);
+    window.alert(multi);
 }
-if (signe="/")
+else if (signe=="/")
 {
-    var resultat3=n1/n2;
-    console.log(resultat3);
-    window.alert(resultat3);
+    var div=n1/n2;
+    console.log(div);
+    window.alert(div);
 }
 else
 {
